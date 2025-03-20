bd22dc56db85c839a2742f0d90d727709e1fb0c7
xuzhean <xuzhean@icloud.com>
2025-03-20 12:54:51 +0800

    test

--- examples/offline_inference_llama.py
+++ examples/offline_inference_llama.py
@@ -49,23 +49,24 @@ num_requests = 3
 for i in range(num_requests):
     prompt = generate_random_prompt(3)
     prompts.append(prompt)
-    # cached_block = []
-    # for layer in range(32):
-    #     cached_block.append(torch.rand(5, 16, 4096, dtype=torch.half))
-    # prompts_cached_block.append(cached_block)
-    
+    cached_block = []
+    for layer in range(32):
+        cached_block.append(torch.rand(2, 16, 4096, dtype=torch.half))
+    prompts_cached_block.append(cached_block)
+
 # 开启 prompt a store 时，不使用 cache 加载
 if os.getenv('ENABLE_PROMPT_A_STORE') == 'True':
     prompts_cached_block = None
 else:
-    prompts_cached_block = []
-    loaded_tensors = torch.load('tmp.pt')
-    num_layers = int(os.getenv('NUM_LAYERS'))
-    for i in range(num_requests):
-        cached_block = []
-        for layer_idx in range(num_layers):
-            cached_block.append(loaded_tensors[str(i) + '.' + str(layer_idx)])
-        prompts_cached_block.append(cached_block)        
+    pass
+    # prompts_cached_block = []
+    # loaded_tensors = torch.load('tmp.pt')
+    # num_layers = int(os.getenv('NUM_LAYERS'))
+    # for i in range(num_requests):
+    #     cached_block = []
+    #     for layer_idx in range(num_layers):
+    #         cached_block.append(loaded_tensors[str(i) + '.' + str(layer_idx)])
+    #     prompts_cached_block.append(cached_block)        
 
 outputs, a_store_dict = llm.generate(prompts, sampling_params, 
                                      prompts_cached_block=prompts_cached_block)
@@ -81,4 +82,3 @@ if a_store_dict is not None:
         for layer_idx in range(num_layers):
             save_tensors_dict[str(id) + '.' + str(layer_idx)] = a_store[layer_idx]
     torch.save(save_tensors_dict, 'tmp.pt')
-            
